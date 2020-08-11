b6050acc4e63ab2283c8feec3e73f9f4b87cc626
fenglisa <lisafeng10@gmail.com>
2020-08-11 11:35:53 -0400

    start welcome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,48 @@
 class UsersController < ApplicationController
 
   # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get "/" do
+    erb :"/index"
   end
 
   # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  get "/signup" do
+    erb :"/users/new"
+  end
+
+  get "/login" do
+    erb :"/users/login"
   end
 
   # POST: /users
   post "/users" do
-    redirect "/users"
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/:id'
+    end
+    redirect '/login'
   end
 
   # GET: /users/5
   get "/users/:id" do
-    erb :"/users/show.html"
+    erb :"/users/home"
   end
 
   # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+  #get "/users/:id/edit" do
+  #  erb :"/users/edit"
+  #end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  #patch "/users/:id" do
+  #  redirect "/users/:id"
+  #end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  #delete "/users/:id/delete" do
+  #  @user = User.find_by_id(params[:id])
+  #  @user.delete
+  #  redirect "/users"
+  #end
 end
